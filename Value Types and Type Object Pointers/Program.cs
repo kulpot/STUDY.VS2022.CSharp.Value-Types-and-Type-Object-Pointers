552ae623c9576f0a609787ba777d93b2428b937c
Kulpot <kulpotwahu@gmail.com>
2023-09-05 22:03:59 +0800

    Add project files.

---
+++ Value Types and Type Object Pointers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=8kkB9l4za-4&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=35
+//
+
+//class Cow
+struct Cow
+{
+    public int mooCount;
+}
+
+class Mainclass
+{
+    static void Main()
+    {
+        Cow betsy = new Cow();
+        betsy.mooCount = 9;
+        betsy.GetType();
+        Cow georgy = new Cow();
+        georgy.mooCount = 3;
+    }
+}
